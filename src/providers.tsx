04cf8f26b07096249799eb587a3b7400e14c7172
AnemonOFF <anemonoff@users.noreply.github.com>
2025-01-23 13:43:01 +0500

    fix query client

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,6 +1,5 @@
 import { lazy, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Toaster } from "./shared/ui/sonner";
 import {
   QueryCache,
   QueryClient,
@@ -9,6 +8,7 @@ import {
 import axios from "axios";
 import { toast } from "sonner";
 import { ErrorPage } from "./pages/error";
+import { Toaster } from "./shared/ui/sonner";
 
 const router = createBrowserRouter([
   {
@@ -54,53 +54,60 @@ const router = createBrowserRouter([
 
 export default function Providers() {
   const [queryClient] = useState(
-    new QueryClient({
-      queryCache: new QueryCache({
-        onError: (err) => {
-          if (axios.isAxiosError(err)) {
-            if (err.response && err.response?.status >= 500)
-              toast.error("Сервер не смог обработать запрос");
-          } else {
-            toast.error("Не удалось выполнить запрос");
-          }
-        },
-      }),
-      defaultOptions: {
-        queries: {
-          staleTime: 1000 * 60 * 5,
-          retry: (count, error) => {
-            if (
-              axios.isAxiosError(error) &&
-              error.response &&
-              error.response.status < 500
-            )
-              return false;
-            if (count >= 3) return false;
-            if (!axios.isAxiosError(error) || !error.response) return true;
+    () =>
+      new QueryClient({
+        queryCache: new QueryCache({
+          onError: (err) => {
+            if (axios.isAxiosError(err)) {
+              if (
+                err.code === "ERR_NETWORK" ||
+                (err.response && err.response?.status >= 500)
+              ) {
+                toast.error("Сервер не смог обработать запрос");
+              }
+            } else {
+              toast.error("Не удалось выполнить запрос");
+            }
+          },
+        }),
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5,
+            retry: (count, error) => {
+              if (
+                axios.isAxiosError(error) &&
+                error.response &&
+                error.response.status < 500
+              )
+                return false;
+              if (count >= 2) return false;
+              if (!axios.isAxiosError(error) || !error.response) return true;
 
-            return (
-              error.response.status !== 401 && error.response.status !== 404
-            );
+              return (
+                error.response.status !== 401 && error.response.status !== 404
+              );
+            },
+          },
+          mutations: {
+            retry: (count, error) => {
+              if (count >= 2) return false;
+              if (!axios.isAxiosError(error) || !error.response) return true;
+
+              return (
+                error.response.status !== 401 && error.response.status !== 404
+              );
+            },
           },
         },
-        mutations: {
-          retry: (count, error) => {
-            if (count >= 3) return false;
-            if (!axios.isAxiosError(error) || !error.response) return true;
-
-            return (
-              error.response.status !== 401 && error.response.status !== 404
-            );
-          },
-        },
-      },
-    })
+      })
   );
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+    <>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
       <Toaster position="top-right" richColors />
-    </QueryClientProvider>
+    </>
   );
 }
